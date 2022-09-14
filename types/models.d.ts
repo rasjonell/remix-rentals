17fbbb0f718791b548a4ac875ebe2a8ccb984cca
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2022-09-14 17:41:32 +0400

    feat(DB): update DB schema

--- types/models.d.ts
+++ types/models.d.ts
@@ -2,7 +2,12 @@ import type { Prisma } from '@prisma/client';
 
 declare global {
   namespace Models {
-    type BikeWithRelationships = Prisma.BikeGetPayload<{ include: { reservations: true } }>;
-    type UserWithRelationships = Prisma.UserGetPayload<{ include: { reservations: true } }>;
+    type BikeWithRelationships = Prisma.BikeGetPayload<{
+      include: { reservations: { include: { User: true } } };
+    }>;
+
+    type UserWithRelationships = Prisma.UserGetPayload<{
+      include: { reservations: { include: { Bike: true } } };
+    }>;
   }
 }
